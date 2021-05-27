8c94962aa2728a1b833391177bdbced49222c129
Syahri Maulana Ramadhan <syahri.maulana@gmail.com>
2021-05-27 16:08:02 +0800

    able to update profpic

--- go-frontend/app.go
+++ go-frontend/app.go
@@ -20,7 +20,11 @@ func main() {
 	r.GET("/login", renderLoginPage)
 	r.GET("/groupchat/list", renderGetJoinedGroupchats)
 	r.GET("/groupchat/explore", renderExplorePage)
+	r.GET("/groupchat/create", renderCreateRoom)
 	r.GET("/profile", renderProfilePage)
+	r.GET("/profile/password", renderChangePassword)
+	r.GET("/profile/username", renderChangeUsername)
+	r.GET("/profile/profpic", renderChangeProfpic)
 	r.Static("/css", "./css")
 	r.Run(":80")
 }
@@ -65,9 +69,45 @@ func renderExplorePage(c *gin.Context) {
 	})
 }
 
+func renderCreateRoom(c *gin.Context) {
+	c.HTML(http.StatusOK, "createroom.html", gin.H{
+		"title":        "Create a Room",
+		"host":         host,
+		"account_host": backendHost,
+		"gc_host":      groupchatBackendHost,
+	})
+}
+
 func renderProfilePage(c *gin.Context) {
 	c.HTML(http.StatusOK, "profile.html", gin.H{
 		"title":        "Profile",
+		"host":         host,
+		"account_host": backendHost,
+		"gc_host":      groupchatBackendHost,
+	})
+}
+
+func renderChangePassword(c *gin.Context) {
+	c.HTML(http.StatusOK, "changepassowrd.html", gin.H{
+		"title":        "Profile | Change Password",
+		"host":         host,
+		"account_host": backendHost,
+		"gc_host":      groupchatBackendHost,
+	})
+}
+
+func renderChangeUsername(c *gin.Context) {
+	c.HTML(http.StatusOK, "changeusername.html", gin.H{
+		"title":        "Profile | Change Username",
+		"host":         host,
+		"account_host": backendHost,
+		"gc_host":      groupchatBackendHost,
+	})
+}
+
+func renderChangeProfpic(c *gin.Context) {
+	c.HTML(http.StatusOK, "changeprofpic.html", gin.H{
+		"title":        "Profile | Change Profile Picture",
 		"host":         host,
 		"account_host": backendHost,
 		"gc_host":      groupchatBackendHost,
